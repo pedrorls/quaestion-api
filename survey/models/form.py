09ad60b75cc7bf87e0887bf64bc42df518115be4
Pedro Silva <pedro.rls93@gmail.com>
2019-11-11 16:07:42 -0300

    form model

---
+++ survey/models/form.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Form(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "form"
+        verbose_name_plural = "forms"
+
+    def __str__(self):
+        return self.title
