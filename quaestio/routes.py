ddaedbef61b70b6e331318ad5492af17ea2589a9
Pedro Silva <pedro.rls93@gmail.com>
2019-11-12 22:10:16 -0300

    add option viewset to router

--- quaestio/routes.py
+++ quaestio/routes.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
-from survey.viewsets import FormViewset, QuestionViewSet
+from survey.viewsets import FormViewset, QuestionViewSet, OptionViewSet
 
 router = routers.DefaultRouter()
 router.register(r"forms", FormViewset)
 router.register(r"questions", QuestionViewSet)
+router.register(r"options", OptionViewSet)
 
