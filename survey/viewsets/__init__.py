e4d9f8101000db567cca0ed2f4be6d918d323bd9
Pedro Silva <pedro.rls93@gmail.com>
2019-11-12 22:18:17 -0300

    create answer viewset

--- survey/viewsets/__init__.py
+++ survey/viewsets/__init__.py
@@ -1,3 +1,4 @@
 from .form import FormViewset
 from .question import QuestionViewSet
 from .option import OptionViewSet
+from .answer import AnswerViewSet
---
+++ survey/viewsets/answer.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from ..models import Answer
+from ..serializers import AnswerSerializer
+
+class AnswerViewSet(viewsets.ModelViewSet):
+
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
