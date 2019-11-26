7937d4762e71f7014074c3f217334b9feaabaded
Pedro Silva <pedro.rls93@gmail.com>
2019-11-26 11:18:35 -0300

    add permission

--- survey/viewsets/form.py
+++ survey/viewsets/form.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from ..models import Form
 from ..serializers import FormSerializer
+from ..permissions import IsCreator
+
 
 class FormViewset(viewsets.ModelViewSet):
     lookup_fields = "id"
     queryset = Form.objects.all()
     serializer_class = FormSerializer
+    permission_classes = [IsCreator]
