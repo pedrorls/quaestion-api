c10cc3b930a9e2505bbb4e2a2934117696a27bf3
Pedro Silva <pedro.rls93@gmail.com>
2020-01-24 16:05:13 -0300

    apply validation and filterset

--- survey/viewsets/form.py
+++ survey/viewsets/form.py
@@ -3,10 +3,13 @@ from django_filters.rest_framework import DjangoFilterBackend
 from ..models import Form
 from ..serializers import FormSerializer
 from ..permissions import IsUserCodeSupplied
+from ..filterset import IsFormCreator
 
 
 class FormViewset(viewsets.ModelViewSet):
     queryset = Form.objects.all()
     serializer_class = FormSerializer
     permission_classes = (IsUserCodeSupplied,)
+    filter_backends = (DjangoFilterBackend, IsFormCreator)
+    filter_fields = ("creator__key",)
 
