c9cbf37cb4bc17d7b98fed8e3247058e2d971bec
Pedro Silva <pedro.rls93@gmail.com>
2020-01-24 16:05:38 -0300

    filter set to return only questions by form

--- survey/filterset.py
+++ survey/filterset.py
@@ -9,3 +9,11 @@ class IsFormCreator(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         code = request.query_params.get("code")
         return queryset.filter(creator__key=code)
+
+class BelongsToCreatedForm(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        code = request.query_params.get("code")
+        form_id = request.query_params.get("form_id")
+
+        form = Form.objects.get(creator__key=code, id=form_id)
+        return queryset.filter(form=form)
