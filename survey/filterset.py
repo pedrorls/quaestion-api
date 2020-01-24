2669e187d2276cf0c9e95a856bdeab8e9ad7d65e
Pedro Silva <pedro.rls93@gmail.com>
2020-01-24 16:41:32 -0300

    create filters for options and answers

--- survey/filterset.py
+++ survey/filterset.py
@@ -1,6 +1,7 @@
+from django.db.models import Q
 from rest_framework.filters import BaseFilterBackend
 
-from .models import Form
+from .models import Question
 
 class IsFormCreator(BaseFilterBackend):
     """
@@ -10,10 +11,20 @@ class IsFormCreator(BaseFilterBackend):
         code = request.query_params.get("code")
         return queryset.filter(creator__key=code)
 
-class BelongsToCreatedForm(BaseFilterBackend):
+class BelongsToForm(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        form_id = request.query_params.get("form_id")
+        return queryset.filter(form__id=form_id)
+
+
+class BelongsToQuestion(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        question_id = request.query_params.get("question_id")
+        return queryset.filter(question__id=question_id)
+
+
+class BelongsToCreatorOrForm(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         code = request.query_params.get("code")
         form_id = request.query_params.get("form_id")
-
-        form = Form.objects.get(creator__key=code, id=form_id)
-        return queryset.filter(form=form)
+        return queryset.filter(Q(creator__key=code)|Q(form__id=form_id))
