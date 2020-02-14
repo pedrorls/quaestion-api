cf54b499c09ac238d94cc3d4623d515b21c76478
Pedro Silva <pedro.rls93@gmail.com>
2020-02-14 15:57:30 -0300

    improvement(question serializer): add validation to check if created questions belongs to owner form

--- survey/serializers/question.py
+++ survey/serializers/question.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from ..models import Question
+from ..models import Question, Form
+from user.models import User
 
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ["id", "description", "form"]
+
+
+    def validate(self, data):
+        request = self.context["request"]
+        code = request.query_params.get("code", None)
+        form_id = data["form"].id
+        forms = Form.objects.filter(creator__key=code).values_list("id", flat=True)
+
+        if form_id not in forms:
+            raise serializers.ValidationError(
+                "Selected form does not belong to you"
+            )
+        
+        return data
+
