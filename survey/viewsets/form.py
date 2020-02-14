e834dbaa14ecb5197111c3245239b067b7752993
Pedro Silva <pedro.rls93@gmail.com>
2020-02-14 16:00:13 -0300

    improvement(form viewset): add user automatically when form is created

--- survey/viewsets/form.py
+++ survey/viewsets/form.py
@@ -1,15 +1,26 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from ..models import Form
 from ..serializers import FormSerializer
 from ..permissions import IsUserCodeSupplied
 from ..filterset import IsFormCreator
+from user.models import User
 
 
 class FormViewset(viewsets.ModelViewSet):
     queryset = Form.objects.all()
     serializer_class = FormSerializer
     permission_classes = (IsUserCodeSupplied,)
-    filter_backends = (DjangoFilterBackend, IsFormCreator)
+    filter_backends = (IsFormCreator,)
     filter_fields = ("creator__key",)
+
+    def perform_create(self, serializer):
+        code = self.request.query_params.get("code", None)
+        try:
+            user = User.objects.get(key=code)
+            serializer.save(creator=user)
+            return Response(status=status.HTTP_201_CREATED)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
